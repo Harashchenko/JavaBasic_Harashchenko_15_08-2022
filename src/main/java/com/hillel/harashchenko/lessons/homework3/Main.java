173194e4b147db5f0fe68b3e866198a6d76c84ca
Harashchenko Olena <9640034@gmail.com>
2022-08-24 15:25:31 +0300

    added homework

--- src/main/java/com/hillel/harashchenko/lessons/homework3/Main.java
+++ src/main/java/com/hillel/harashchenko/lessons/homework3/Main.java
@@ -1,4 +1,17 @@
 package com.hillel.harashchenko.lessons.homework3;
 
 public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int volume = a * b * c;
+        int length = 4 * (a + b + c);
+
+        System.out.println("The volume of the parallelepiped is " + volume);
+        System.out.println("The total length of the sides of the parallelepiped is " + length);
+
+    }
 }
