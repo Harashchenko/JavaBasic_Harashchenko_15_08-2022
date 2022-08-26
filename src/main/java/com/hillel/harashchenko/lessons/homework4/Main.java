5a45b1f6eec1eaeeeff2ece19905465e40bef202
Harashchenko Olena <9640034@gmail.com>
2022-08-26 17:24:39 +0300

    added homework

---
+++ src/main/java/com/hillel/harashchenko/lessons/homework4/Main.java
@@ -0,0 +1,20 @@
+package com.hillel.harashchenko.lessons.homework4;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a1 = 13;
+        int a2 = 24;
+        int a3 = 46;
+
+        int b1= 9;
+        int b2 = 35;
+        int b3 = 12;
+
+        System.out.println("General attack rate of the Li dynasty " + (a1 + a2 + a3) * 860)
+
+
+
+    }
+}
