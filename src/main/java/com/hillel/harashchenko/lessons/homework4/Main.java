e221df4b9e896d661faf498dbdef4ea22b447d04
Harashchenko Olena <9640034@gmail.com>
2022-08-27 15:24:53 +0300

    added homework

--- src/main/java/com/hillel/harashchenko/lessons/homework4/Main.java
+++ src/main/java/com/hillel/harashchenko/lessons/homework4/Main.java
@@ -7,12 +7,15 @@ public class Main {
         int a1 = 13;
         int a2 = 24;
         int a3 = 46;
+        int resultA = (a1 + a2 + a3) * 860;
 
         int b1= 9;
         int b2 = 35;
         int b3 = 12;
+        double resultB = (b1 + b2 + b3) * 860 * 1.5;
 
-        System.out.println("General attack rate of the Li dynasty " + (a1 + a2 + a3) * 860)
+        System.out.println("Total score attack rate of the Li dynasty " + resultA);
+        System.out.println("Total score attack rate of the Min dynasty " + resultB);
 
 
 
