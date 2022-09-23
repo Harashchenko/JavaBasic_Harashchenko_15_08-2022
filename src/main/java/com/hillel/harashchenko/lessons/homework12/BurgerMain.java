986abdc7898a222994b62355372b70875302c8a6
Harashchenko Olena <9640034@gmail.com>
2022-09-23 13:33:21 +0300

    added homework

--- src/main/java/com/hillel/harashchenko/lessons/homework12/BurgerMain.java
+++ src/main/java/com/hillel/harashchenko/lessons/homework12/BurgerMain.java
@@ -4,7 +4,7 @@ public class BurgerMain extends Burger {
     public static void main(String[] args) {
         Burger regularBurger = new Burger("bun", "meat", "greens", "mayo");
         Burger dietBurger = new Burger("bun", "meat", "greens");
-        Burger doubleBurger = new Burger("bun", "meat", "greens", "mayo",true);
+        Burger doubleBurger = new Burger("bun", "meat", "greens", "mayo", true);
 
 
     }
