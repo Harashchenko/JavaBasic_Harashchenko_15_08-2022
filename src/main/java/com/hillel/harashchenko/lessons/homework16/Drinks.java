1a5aaf4986c672c01e16f82dd5c16dcbe0b1f6dc
Harashchenko Olena <9640034@gmail.com>
2022-10-06 12:15:04 +0300

    added homework

---
+++ src/main/java/com/hillel/harashchenko/lessons/homework16/Drinks.java
@@ -0,0 +1,11 @@
+package com.hillel.harashchenko.lessons.homework16;
+
+public class Drinks {
+    public static final int COST_COFFEE = 50;
+    public static final int COST_TEA = 40;
+    public static final int COST_LEMONADE = 20;
+    public static final int COST_MOJITO = 100;
+    public static final int COST_WATER = 10;
+    public static final int COST_COLA = 30;
+
+}
